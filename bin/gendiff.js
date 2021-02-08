5f6ddfb06657e4766df0d4cb815a8c8a7a707df0
mn81566 <mikhail.taskaev@gmail.com>
2021-02-08 14:17:53 +0300

    first

---
+++ bin/gendiff.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+//const { program } = require('commander');
+//program.version('0.0.1');
+//
+//program
+//  .option('-f, --foo', 'enable some foo')
+
+
+// const { Command } = require('commander'); // include commander in git clone of commander repo
+import { Command } from 'commander';
+const program = new Command();
+program.version('0.0.1');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  // .option('-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+  // program.addHelpText('before', `
+  // Example call:
+  // $ custom-help --help`);
+
+// const parseText = process.argv;
+program.parse(process.argv);
