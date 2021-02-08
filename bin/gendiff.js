2a50e48fddc0de1bd8426768d13523921e822583
mn81566 <mikhail.taskaev@gmail.com>
2021-02-08 16:23:51 +0300

    Шаг 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,8 +8,10 @@ program.version('0.0.1');
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
   // .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 // program.addHelpText('before', `
 // Example call:
