d5a30c17ee73ef1928a9c49854c844902ca84289
mn81566 <mikhail.taskaev@gmail.com>
2021-02-10 13:58:37 +0300

    Шаг 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,19 +2,22 @@
 
 // const { Command } = require('commander'); // include commander in git clone of commander repo
 import { Command } from 'commander';
+import runDiffData from '../src/genDiffData.js';
 
 const program = new Command();
 program.version('0.0.1');
-
 program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   // .option('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format');
-
-// program.addHelpText('before', `
-// Example call:
-// $ custom-help --help`);
-
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(runDiffData(filepath1, filepath2));
+  });
 program.parse(process.argv);
+
+// fs.readFile('./filepath.json');
+// fs.open('./filepath.json');
+// console.log(fs.readFileSync('filepath1.json'));
+// console.log(JSON.parse(fs.readFileSync('filepath1.json', 'utf8')));
---
+++ src/genDiffData.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const compare = (obj1, obj2) => {
+  const entries1 = Object.entries(obj1);
+  const entries2 = Object.entries(obj2);
+
+  const unionRes = _.union(entries1, entries2);
+  const uniqRes = _.uniqWith(unionRes, _.isEqual);
+  return _.sortBy(uniqRes, [(o) => o[0]]);
+};
+
+const formattedCompareRes = (obj1, obj2, compareRes) => {
+  const res = compareRes.map(([key, value]) => {
+    if (obj1[key] === value && obj2[key] !== value) {
+      const newKey = `- ${key}`;
+      return [newKey, value];
+    }
+    if (obj1[key] !== value && obj2[key] === value) {
+      const newKey = `+ ${key}`;
+      return [newKey, value];
+    }
+    // if (obj1[key] === value && obj2[key] === value) {
+    const newKey = `  ${key}`;
+    return [newKey, value];
+    // }
+  });
+
+  return res;
+};
+
+const compareToString = (compareRes) => {
+  let res = '';
+  compareRes.forEach(([key, value]) => {
+    res += `  ${key}: ${value}\n`;
+  });
+  return `{\n${res}}`;
+};
+
+const initDiff = (filepath1, filepath2) => {
+  const filepathResolved1 = path.resolve(process.cwd(), filepath1);
+  const filepathResolved2 = path.resolve(process.cwd(), filepath2);
+
+  const dataFile1 = JSON.parse(fs.readFileSync(filepathResolved1, 'utf-8'));
+  const dataFile2 = JSON.parse(fs.readFileSync(filepathResolved2, 'utf-8'));
+
+  const compareRes = compare(dataFile1, dataFile2);
+  const compareResFormatted = formattedCompareRes(dataFile1, dataFile2, compareRes);
+
+  return compareToString(compareResFormatted);
+};
+
+export default initDiff;
