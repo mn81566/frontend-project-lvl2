8570dbe898bf9a5bca85a46a5e68aa6cc7e57baa
mn81566 <mikhail.taskaev@gmail.com>
2021-02-11 19:04:03 +0300

    Шаг 4. Tmp5

--- __tests__/genDiffData.test.js
+++ __tests__/genDiffData.test.js
@@ -1,20 +1,23 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import initDiff from '../src/genDiffData.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const filePath1 = getFixturePath('filepath1.json');
-const filePath2 = getFixturePath('filepath2.json');
+
+const filePath1 = getFixturePath('file1.json');
+const filePath2 = getFixturePath('file2.json');
 const str = `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }`;
 
 test('genDiffData', () => {
-//   const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
   expect(initDiff(filePath1, filePath2)).toEqual(str);
 });
